edb050381530e1aee9646217f16f8dc22ae5abfe
Jérémie Lesage <jeremie.lesage@jeci.fr>
2018-06-14 02:11:01 +0200

    default sub nav widgets

--- src/main/amp/config/alfresco/web-extension/site-webscripts/dk/magenta/header/share-header.get.js
+++ src/main/amp/config/alfresco/web-extension/site-webscripts/dk/magenta/header/share-header.get.js
@@ -1,25 +1,18 @@
+<import resource="classpath:/alfresco/site-webscripts/org/alfresco/share/imports/share-header.lib.js">
+
 var header = widgetUtils.findObject(model.jsonModel.widgets, "id", "HEADER_TITLE_BAR");
 var titleBar = widgetUtils.findObject(model.jsonModel.widgets, "id", "HEADER_TITLE_MENU");
 var navMenu = widgetUtils.findObject(model.jsonModel.widgets, "id", "HEADER_NAVIGATION_MENU_BAR");
 
+/*
 if (titleBar) {
     widgetUtils.deleteObjectFromArray(header.config.widgets, "id", "HEADER_TITLE_MENU");
     delete titleBar;
-
 }
+*/
 
 if (navMenu){
     logger.log('--- Found Nav menu ---');
-    navMenu.config.widgets = [{
-        id: "HEADER_NAVIGATION_BACK_TO_DOC_PREVIEW",
-        name: "libreofficepage/AlfMenuBarItem",
-        config: {
-            id: "HEADER_NAVIGATION_BACK_TO_DOC_PREVIEW",
-            label: msg.get("header.back.to.preview"),
-            iconClass: "alf-back-icon",
-            targetUrl: "#back",
-            selected: false
-        }
-    }];
+    navMenu.config.widgets = getSubNavigationWidgets();
 
 }
